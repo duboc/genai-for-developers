b8ae31b51e38b98b57c2574614aecd68646e1463
duboc <andersonduboc@gmail.com>
2024-10-03 20:17:26 +0000

    vai

--- devai-cli/src/devai/commands/review.py
+++ devai-cli/src/devai/commands/review.py
@@ -35,7 +35,7 @@ from rich.table import Table
 
 # Uncomment after configuring JIRA and GitLab env variables - see README.md for details
 
-# from devai.commands.jira import create_jira_issue
+from devai.commands.jira import create_jira_issue
 # from devai.commands.gitlab import create_gitlab_issue_comment
 
 from .constants import USER_AGENT, MODEL_NAME
@@ -352,7 +352,7 @@ Provide an overview or overall impression entry for the code as the first entry.
     else:
         click.echo(response.text) 
 
-    #create_jira_issue("Code Review Results", response.text)
+    create_jira_issue("Code Review Results", response.text)
     # create_gitlab_issue_comment(response.text)
 
 
